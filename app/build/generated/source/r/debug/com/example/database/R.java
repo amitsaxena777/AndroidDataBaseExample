1951e4cbbadf82b37e96314606ff0a284e6a6819
rk250268 <Ravinder.Kundarapu@ncr.com>
2016-09-11 13:52:23 +0530

    Database Code for Android

--- app/build/generated/source/r/debug/com/example/database/R.java
+++ app/build/generated/source/r/debug/com/example/database/R.java
@@ -34,8 +34,7 @@ public final class R {
     public static final class layout {
         public static final int activity_book=0x7f030000;
         public static final int activity_main=0x7f030001;
-        public static final int activity_main2=0x7f030002;
-        public static final int row_layout=0x7f030003;
+        public static final int row_layout=0x7f030002;
     }
     public static final class menu {
         public static final int menu_main2=0x7f060000;
